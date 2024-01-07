ff3d2190c84589146c87c0d1e7b811059c008b37
trong.dang <dtrong97vn@gmail.com>
2024-01-07 09:58:39 +0700

    feat: update truffle config

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -4,11 +4,17 @@ const Web3 = require("web3");
 
 module.exports = {
   networks: {
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      gas: 5000000,
+      network_id: "*",
+    },
     mumbai: {
       provider: () =>
         new HDWalletProvider({
           mnemonic: process.env.MNEMONIC,
-          providerOrUrl: 'wss://polygon-mumbai-bor.publicnode.com	',
+          providerOrUrl: "wss://polygon-mumbai-bor.publicnode.com",
           addressIndex: 0,
           numberOfAddresses: 10,
         }),
